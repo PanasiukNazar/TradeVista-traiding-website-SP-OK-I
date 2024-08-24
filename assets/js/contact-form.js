9679884e881234ffac7c722e803a701120e07b48
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2024-08-24 12:06:03 +0300

    DONE

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,12 +1,12 @@
-const submit = document.querySelector('#submit')
+const submit = document.querySelector('#submit');
 
 submit.addEventListener('click', () => {
-    event.preventDefault()
-})
+    event.preventDefault();
+});
 
 function sendForm() {
     const newPath = 'thanks.html';
-    
+
     // Get the input value
     const emailInput = document.getElementById('email').value;
 
@@ -18,6 +18,45 @@ function sendForm() {
         document.getElementById('result').textContent = '';
         window.location.href = newPath;
     } else {
-        document.getElementById('result').textContent = 'Неверный адрес электронной почты. Пожалуйста, введите действительный адрес электронной почты.';
+        document.getElementById('result').textContent =
+            'Неверный адрес электронной почты. Пожалуйста, введите действительный адрес электронной почты.';
+    }
+}
+
+function subscribe() {
+    const newPath = 'thanks.html';
+
+    // Get the input value
+    const emailInput = document.getElementById('newsletter').value;
+
+    // Regular expression for basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Test the input against the regular expression
+    if (emailRegex.test(emailInput)) {
+        document.getElementById('validation').textContent = '';
+        window.location.href = newPath;
+    } else {
+        document.getElementById('validation').textContent =
+            'Неверный адрес электронной почты. Пожалуйста, введите действительный адрес электронной почты.';
+    }
+}
+
+function sendApply() {
+    const newPath = 'thanks.html';
+
+    // Get the input value
+    const emailInput = document.getElementById('apply').value;
+
+    // Regular expression for basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Test the input against the regular expression
+    if (emailRegex.test(emailInput)) {
+        document.getElementById('result1').textContent = '';
+        window.location.href = newPath;
+    } else {
+        document.getElementById('result1').textContent =
+            'Неверный адрес электронной почты. Пожалуйста, введите действительный адрес электронной почты.';
     }
 }
